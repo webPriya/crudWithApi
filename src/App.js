dfc4813d9940fcedd2fd1a63521f8df758bf5c92
priya1019 <priyanka.c@purpleslate.in>
2022-03-28 15:54:02 +0530

    crud operation using apis

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {BrowserRouter,Switch ,Route} from "react-router-dom"
+import Home from "./components/pages/Home";
+import View from "./components/student/View"
+import Edit from './components/student/Edit'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  
+        <BrowserRouter>
+        <Switch>
+    {/* <Routes> */}
+      <Route exact path="/" component={Home} />
+      <Route  exact path="/view/:id" component={View} />
+      <Route path="/edit/:id" component={Edit} />
+      {/* <Route path="about" element={<About />} /> */}
+      </Switch>
+    {/* </Routes> */}
+  </BrowserRouter>
+
+     
+    
   );
 }
 
